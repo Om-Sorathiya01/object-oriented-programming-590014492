e6ea1fc053a285fe4037f468d5cb568629690654
Om Sorathiya <om.sorathiya@example.com>
2026-01-22 08:41:13 +0530

    Add Scanner function for user input in car.java

---
+++ car.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class car {
+    int modelYear;
+    int mileage;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        car c1 = new car();
+        car c2 = new car();
+        
+        System.out.print("Enter Car 1 Model Year: ");
+        c1.modelYear = scanner.nextInt();
+        
+        System.out.print("Enter Car 1 Mileage: ");
+        c1.mileage = scanner.nextInt();
+        
+        System.out.print("Enter Car 2 Model Year: ");
+        c2.modelYear = scanner.nextInt();
+        
+        System.out.print("Enter Car 2 Mileage: ");
+        c2.mileage = scanner.nextInt();
+        
+        System.out.println("Car 1 Model Year: " + c1.modelYear + ", Mileage: " + c1.mileage);
+        System.out.println("Car 2 Model Year: " + c2.modelYear + ", Mileage: " + c2.mileage);
+        
+        scanner.close();
+    }
+}
