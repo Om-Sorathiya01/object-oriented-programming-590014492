b7388a238ff45d5e8d6e14a750d8ce47b6324d3f
Om Sorathiya <om.sorathiya@example.com>
2026-01-29 12:32:39 +0530

    Initial commit: Java assignment files

---
+++ lansta1.java
@@ -0,0 +1,13 @@
+public class Labsta {
+   int m;
+   public static void main(String args[]) {
+      java.util.Scanner s = new java.util.Scanner(System.in);
+      Labsta n = new Labsta();
+      n.m = s.nextInt();
+        System.out.println("Value of m: " + n.m);
+   }
+
+
+
+}
+
---
+++ lansta2.java
@@ -0,0 +1,9 @@
+public class lansta2 {
+    public static void main(String args[]) {
+        java.util.Scanner s = new java.util.Scanner(System.in);
+        int n;
+        n = s.nextInt();
+        System.out.println("Value of n: " + n);
+     }
+     int m;
+}
