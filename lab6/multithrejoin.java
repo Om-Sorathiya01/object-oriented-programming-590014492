42fa875691439d8470e991a2728d3c5c0caf40e2
Om Sorathiya <om.sorathiya@example.com>
2026-02-26 12:17:58 +0530

    Add lab 6 files

---
+++ lab6/multithrejoin.java
@@ -0,0 +1,50 @@
+package lab6;
+
+class Thread1 extends Thread {
+    public void run() {
+        System.out.println("Thread 1 started");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread 1: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Thread 1 finished");
+    }
+}
+
+class Thread2 extends Thread {
+    public void run() {
+        System.out.println("Thread 2 started");
+        for (int i = 6; i <= 10; i++) {
+            System.out.println("Thread 2: " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Thread 2 finished");
+    }
+}
+
+public class multithrejoin {
+    public static void main(String[] args) {
+        Thread1 t1 = new Thread1();
+        Thread2 t2 = new Thread2();
+        
+        t1.start();
+        t2.start();
+        
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("Main thread finished");
+    }
+}
